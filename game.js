dd246f7ebdc49d74991a58e2a549a65c33a9c0cd
WilmaNyren <nywi24wv@student.ju.se>
2024-11-22 14:58:28 +0100

    class funkar yay :)) moving up

--- game.js
+++ game.js
@@ -1,33 +1,44 @@
-let position = {
-  x:100,
-  y:100
-};
+//let position = {
+//x:250,
+//y:150
+//};
 
 function setup() {
   createCanvas(500, 300);
   rectMode(CENTER);
 }
+class FireMan {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    fill(0);
+    ellipse(this.x, this.y, 15);
+  }
+
+  update() {
+    this.y += -1;
+  }
+}
+
+
+
+let fireMan = new FireMan(250, 150);
 
 function draw() {
   backG();
-  //FireMan.draw();
-  fill (0);
-    ellipse(position.x,position.y,100);
-  
+  //moving(not yet) dot
+  fireMan.draw();
+  fireMan.update();
+  //fill (0);
+  //ellipse(position.x,position.y,20);
 }
 
-function backG(){
+function backG() {
   background(100);
-  fill (200);
+  fill(200);
   noStroke();
-  rect(250,150,300,300);
+  rect(250, 150, 300, 300);
 }
-
- //class FireMan{
- // update(){}
- // draw(){
-   // fill (0);
-    //ellipse(position.x,position.y,100);
-  //}
-
- //}
